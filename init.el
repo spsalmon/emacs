427b68e7bb5e9f1d13e6374a71acb541762e6645
sacha <sacha.psalmon@unibe.ch>
2024-08-16 09:56:45 +0200

    removed typescript and added python

--- init.el
+++ init.el
@@ -388,3 +388,16 @@
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(rainbow-delimiters forge magit counsel-projectile projectile company-box company typescript-mode lsp-ivy lsp-treemacs lsp-ui lsp-mode visual-fill-column org-bullets hydra helpful counsel ivy-rich ivy which-key doom-modeline all-the-icons doom-themes use-package)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
