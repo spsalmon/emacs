0466a8b6abcc32f2165f9dd749b6d1d09a6ee94f
spsalmon <psalmonsacha@gmail.com>
2024-08-17 10:24:41 +0200

    added basic python support, still doesnt show errors

--- init.el
+++ init.el
@@ -413,3 +413,16 @@
 
 (use-package rainbow-delimiters
   :hook (prog-mode . rainbow-delimiters-mode))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(general which-key visual-fill-column typescript-mode rainbow-delimiters python-mode org-bullets lsp-ui lsp-ivy ivy-rich helpful forge doom-themes doom-modeline dap-mode counsel-projectile company-box all-the-icons)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
